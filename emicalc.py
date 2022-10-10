dce0b06a68d74bc72248970c7f93f7825ca65896
Cibin Joseph <34376424+cibinjoseph@users.noreply.github.com>
2022-10-10 16:37:24 +0530

    Add files via upload

---
+++ emicalc.py
@@ -0,0 +1,35 @@
+
+# Set no. of iterations
+niter = 15
+
+# Set initial approx. rate of interest
+xInitial = 1.10
+
+def getA(P, r, n):
+    """ Function to return amount """
+    numer = P*r*(1.0+r)**n
+    denom = ((1.0+r)**n)-1.0
+    return numer/denom
+
+def getr(P, A, n):
+    """ Function to obtain rate of interest """
+    x = xInitial
+    for i in range(niter):
+        print(i, end="  ")
+        print(x)
+        numer = P*x**(n+1.0)-x**n*(A+P)+A
+        denom = (n+1.0)*P*x**n-(A+P)*n*x**(n-1)
+        x = x - numer/denom
+
+    return x-1.0
+
+# Sample problem
+P = 10000.0
+r = 0.16
+n = 2.0
+A = getA(P, r, n)
+print("Amount: " + str(A))
+
+rOut = getr(P, A, n)
+print("Computed rate: " + str(rOut))
+
